818edfca7ce7f73df145b7fedcb17508f4b174af
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-07-20 13:50:08 +0530

    create user routes and controllers

---
+++ server/routes/users.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getUser, getUserInfo, updateUser } from "../controllers/users.js";
+
+const router = express.Router();
+
+router.get("/me/info/", getUserInfo);
+router.get("/me", getUser);
+router.put("/me", updateUser);
+
+export default router;
