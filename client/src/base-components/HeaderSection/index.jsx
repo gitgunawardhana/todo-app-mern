b79d0ce2ba329b8a8500bc7598ccbde1101513f5
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-07-20 13:39:52 +0530

    add profile dropdown

--- client/src/base-components/HeaderSection/index.jsx
+++ client/src/base-components/HeaderSection/index.jsx
@@ -1,17 +1,30 @@
+import { useContext, useState } from "react";
 import { twMerge } from "tailwind-merge";
+import ProfileDropdown from "../../components/ProfileDropdown";
+import { ProviderContext } from "../../components/Provider";
 
 const HeaderSection = (props) => {
+  const { token, user } = useContext(ProviderContext);
+  const [showProfile, setShowProfile] = useState(false);
+
   return (
     <>
       <header
         className={twMerge([
-          "w-full justify-center bg-transparent pt-8 text-center duration-100 ",
+          "flex w-full justify-center bg-transparent pt-8 text-center duration-100",
           props.className,
         ])}
       >
         <h1 className="py-1 pb-5 text-3xl font-bold text-[#f59e0b] dark:text-[#f59e0b]">
           {props.children}
         </h1>
+        {user !== null && token !== null && (
+          <div className="flex">
+            <div className="absolute right-[2rem] top-[2.5rem]">
+              <ProfileDropdown />
+            </div>
+          </div>
+        )}
       </header>
     </>
   );
---
+++ client/src/components/ProfileDropdown/index.jsx
@@ -0,0 +1,68 @@
+import { useContext } from "react";
+import { twMerge } from "tailwind-merge";
+import BadgeAvatars from "../../base-components/BadgeAvatars";
+import DropdownButton from "../../base-components/DropdownButton";
+import HomeSvg from "../../base-components/SvgIcon/HomeSvg";
+import ProfileSvg from "../../base-components/SvgIcon/ProfileSvg";
+import { Modes } from "../../constants";
+import DropdownOptions from "../DropdownOptions";
+import { ProviderContext } from "../Provider";
+
+const ProfileDropdown = () => {
+  const { theme, setTheme } = useContext(ProviderContext);
+
+  const profileSvg = (
+    <ProfileSvg
+      className={twMerge([
+        "w-4 stroke-[#94A3B8] group-hover:stroke-[#f59e0b]",
+        theme === Modes.LIGHT && "stroke-[#f59e0b]",
+      ])}
+    />
+  );
+
+  const homeSvg = (
+    <HomeSvg
+      className={twMerge([
+        "w-4 stroke-[#94A3B8] group-hover:stroke-[#f59e0b]",
+        theme === Modes.DARK && "stroke-[#f59e0b]",
+      ])}
+    />
+  );
+
+  const profileOptionList = [
+    {
+      icon: homeSvg,
+      text: "Home",
+      to: "/",
+    },
+    {
+      icon: profileSvg,
+      text: "Profile",
+      to: "/profile",
+    },
+  ];
+  return (
+    <div>
+      <DropdownButton
+        id="profileSetting"
+        items={profileOptionList}
+        buttonClassName="!bg-red z-50 inline-flex items-center text-center focus:outline-none focus:ring-0 hover:bg-transparent dark:!bg-transparent dark:hover:!bg-transparent !bg-transparent"
+        icon={<BadgeAvatars online size="!h-12 !w-12" />}
+        className="absolute right-0 top-16 z-50"
+      >
+        {profileOptionList.map((item) => (
+          <DropdownButton.LI key={item.text} className="group z-50 w-fit">
+            <DropdownOptions
+              to={item.to}
+              className="w-28"
+              label={item.text}
+              icon={item.icon}
+            />
+          </DropdownButton.LI>
+        ))}
+      </DropdownButton>
+    </div>
+  );
+};
+
+export default ProfileDropdown;
