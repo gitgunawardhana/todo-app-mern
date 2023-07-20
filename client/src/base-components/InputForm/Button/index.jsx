2e11e4592d11d31a0199c23703342e3f7dc69f5e
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-07-20 13:39:26 +0530

    Update index.jsx

--- client/src/base-components/InputForm/Button/index.jsx
+++ client/src/base-components/InputForm/Button/index.jsx
@@ -5,15 +5,14 @@ const Button = (props) => {
     <>
       <button
         id={props.id && props.id}
-        data-dropdown-toggle={
-          props.dataDropdownToggle && props.dataDropdownToggle
-        }
         type={props.type ? props.type : "button"}
         onClick={props.onClick && props.onClick}
         className={twMerge([
           "flex items-center justify-between rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium leading-5 text-white hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700",
           props.className,
+          props.disabled && "disabled opacity-50 hover:!bg-[#fbbf24]",
         ])}
+        disabled={props.disabled && props.disabled}
       >
         {props.children}
       </button>
