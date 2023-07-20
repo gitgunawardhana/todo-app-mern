513e6f15d7ba2db3b28c00a3b82e27f91dd94dfc
Ishan Tharindu <t.gunawardana864@gmail.com>
2023-07-20 13:39:30 +0530

    Update index.jsx

--- client/src/base-components/InputForm/InputField/index.jsx
+++ client/src/base-components/InputForm/InputField/index.jsx
@@ -6,8 +6,8 @@ const InputField = (props) => {
       <div className="w-full">
         <div className="relative">
           <input
-            type="text"
-            id="small_outlined"
+            type={props.type ? props.type : "text"}
+            id={props.id && props.id}
             className={twMerge([
               "border-1 peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-1.5 pt-3 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500",
               props.className,
@@ -15,9 +15,12 @@ const InputField = (props) => {
             placeholder={props.placeholder && props.placeholder}
             onChange={props.onChange}
             value={props.value}
+            disabled={props.disabled && props.disabled}
+            name={props.name && props.name}
+            accept={props.accept && props.accept}
           />
           <label
-            htmlFor="small_outlined"
+            htmlFor={props.id && props.id}
             className={twMerge([
               "absolute left-1 top-1 z-10 origin-[0] -translate-y-3 scale-75 transform bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-1 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 dark:bg-gray-900 dark:text-gray-400 peer-focus:dark:text-blue-500",
               props.labelClassName,
